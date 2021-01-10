ceabebb22efd1ff83154ffeb27bb9e998e2b52ab
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-01-11 00:36:48 +0500

    Dilnoz add

--- app/src/main/java/uz/unzosoft/fastfoodbecasel/ui/onboarding/Fragment3.kt
+++ app/src/main/java/uz/unzosoft/fastfoodbecasel/ui/onboarding/Fragment3.kt
@@ -2,28 +2,32 @@ package uz.unzosoft.fastfoodbecasel.ui.onboarding
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_on_boarding3.*
+import kotlinx.android.synthetic.main.fragment_on_boarding3.view.*
 import uz.unzosoft.fastfoodbecasel.MainActivity
 import uz.unzosoft.fastfoodbecasel.R
 
 @Suppress("UNREACHABLE_CODE")
-class Fragment3 : Fragment() {
+class Fragment3 : Fragment(R.layout.fragment_on_boarding3) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_on_boarding3, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         skip.setOnClickListener {
             startActivity(Intent(this.context, MainActivity::class.java))
-            Toast.makeText(this.context, "Click!!!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this.context, "Create", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun v(v: View) {
+        skip.setOnClickListener {
+            startActivity(Intent(this.context, MainActivity::class.java))
+            Toast.makeText(this.context, "Create", Toast.LENGTH_SHORT).show()
         }
     }
 }
