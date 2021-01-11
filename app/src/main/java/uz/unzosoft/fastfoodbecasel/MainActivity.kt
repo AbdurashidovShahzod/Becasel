54e8c896d188a96bf6b1e1d96082d4e5f1969b9e
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-01-11 06:32:01 +0500

    Appetizers and combo categories

--- app/src/main/java/uz/unzosoft/fastfoodbecasel/MainActivity.kt
+++ app/src/main/java/uz/unzosoft/fastfoodbecasel/MainActivity.kt
@@ -3,7 +3,6 @@ package uz.unzosoft.fastfoodbecasel
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_main.*
 import uz.unzosoft.fastfoodbecasel.adapters.FoodAdapter
 import uz.unzosoft.fastfoodbecasel.adapters.AsiaFoodAdapter
@@ -14,12 +13,14 @@ import uz.unzosoft.fastfoodbecasel.ui.model.CategoriesFood
 class MainActivity : AppCompatActivity() {
     lateinit var asiaFoodAdapter: AsiaFoodAdapter
     lateinit var foodAdapter: FoodAdapter
-    var listFood = ArrayList<LoookFood>()
     var listPopular = ArrayList<CategoriesFood>()
+
+    var listBurger = ArrayList<LoookFood>()
     var listChicken = ArrayList<LoookFood>()
     var listSpinners = ArrayList<LoookFood>()
     var listPizza = ArrayList<LoookFood>()
     var listDrinks = ArrayList<LoookFood>()
+    var listAppetizersCombo = ArrayList<LoookFood>()
 
 
     override
@@ -38,80 +39,80 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initData() {
-        listFood.add(LoookFood("BIGGER", "16 000 UZS", R.drawable.a1, "5.0", "Becausel"))
-        listFood.add(LoookFood("CLASSIC", "12 000 UZS", R.drawable.classic, "5.0", "Becausel"))
-        listFood.add(
+        listBurger.add(LoookFood("BIGGER", "16 000 UZS", R.drawable.a1, "5.0", "Becasel"))
+        listBurger.add(LoookFood("CLASSIC", "12 000 UZS", R.drawable.classic, "5.0", "Becasel"))
+        listBurger.add(
             LoookFood(
                 "CHEESEBURGER",
                 "24 000 UZS",
                 R.drawable.chesbirger,
                 "4.8",
-                "Becausel"
+                "Becasel"
             )
         )
-        listFood.add(
+        listBurger.add(
             LoookFood(
                 "CHILI LONGER",
                 "21 000 UZS",
                 R.drawable.chililonger,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
-        listFood.add(
+        listBurger.add(
             LoookFood(
-                "LOOOK", "14 000 UZS", R.drawable.look, "4.5", "Becausel"
+                "LOOOK", "14 000 UZS", R.drawable.look, "4.5", "Becasel"
             )
         )
-        listFood.add(
+        listBurger.add(
             LoookFood(
                 "CHICKEN LONGER",
                 "14 000 UZS",
                 R.drawable.chickenloger,
                 "3.8",
-                "Becausel"
+                "Becasel"
             )
         )
-        listFood.add(
+        listBurger.add(
             LoookFood(
                 "HAMBURGER",
-                "21 000 UZS", R.drawable.humbergers, "4.6", "Becausel"
+                "21 000 UZS", R.drawable.humbergers, "4.6", "Becasel"
             )
         )
-        listFood.add(
+        listBurger.add(
             LoookFood(
                 "DOUBLECHEESE BURGER",
                 "34 000 UZS",
                 R.drawable.doubleches,
                 "4.7",
-                "Becausel"
+                "Becasel"
             )
         )
-        listFood.add(
+        listBurger.add(
             LoookFood(
                 "BEEF LONGER",
                 "21 000 UZS",
                 R.drawable.beeflonger,
                 "3.8",
-                "Becausel"
+                "Becasel"
             )
         )
-        listFood.add(
+        listBurger.add(
             LoookFood(
                 "CRISPY BURGER",
                 "15 000 UZS",
                 R.drawable.cripsy,
                 "2.9",
-                "Becausel"
+                "Becasel"
             )
         )
-        listFood.add(
+        listBurger.add(
             LoookFood(
                 "BOSS BURGER",
                 "34 000 UZS",
                 R.drawable.bossburger,
                 "3.9",
-                "Becausel"
+                "Becasel"
             )
         )
 
@@ -138,7 +139,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel",
+                "Becasel",
             )
         )
         listChicken.add(
@@ -147,7 +148,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -156,7 +157,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -165,7 +166,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -174,7 +175,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -183,7 +184,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -192,7 +193,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -201,7 +202,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -210,7 +211,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -219,7 +220,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -228,7 +229,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -237,7 +238,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listChicken.add(
@@ -246,7 +247,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         /**
@@ -258,7 +259,7 @@ class MainActivity : AppCompatActivity() {
                 "12 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listSpinners.add(
@@ -267,7 +268,7 @@ class MainActivity : AppCompatActivity() {
                 "16 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listSpinners.add(
@@ -276,7 +277,7 @@ class MainActivity : AppCompatActivity() {
                 "18 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listSpinners.add(
@@ -285,7 +286,7 @@ class MainActivity : AppCompatActivity() {
                 "12 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listSpinners.add(
@@ -294,7 +295,7 @@ class MainActivity : AppCompatActivity() {
                 "12 000 UZS",
                 R.drawable.spinner,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
 
@@ -307,7 +308,7 @@ class MainActivity : AppCompatActivity() {
                 "39 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -316,7 +317,7 @@ class MainActivity : AppCompatActivity() {
                 "29 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -325,7 +326,7 @@ class MainActivity : AppCompatActivity() {
                 "49 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -334,7 +335,7 @@ class MainActivity : AppCompatActivity() {
                 "33 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -343,7 +344,7 @@ class MainActivity : AppCompatActivity() {
                 "32 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -352,7 +353,7 @@ class MainActivity : AppCompatActivity() {
                 "26 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -361,7 +362,7 @@ class MainActivity : AppCompatActivity() {
                 "21 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -370,7 +371,7 @@ class MainActivity : AppCompatActivity() {
                 "31 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -379,7 +380,7 @@ class MainActivity : AppCompatActivity() {
                 "25 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         listPizza.add(
@@ -388,32 +389,120 @@ class MainActivity : AppCompatActivity() {
                 "32 000 UZS",
                 R.drawable.popularfood3,
                 "5.0",
-                "Becausel"
+                "Becasel"
             )
         )
         /**
          * Drink food
          */
-        listDrinks.add(LoookFood("MINERALKA 0.5L BEZGAZ","3 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("MINERALKA 1.5L BEZGAZ","5 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("MINERALKA 1L BEZGAZ","4 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("MINERALKA 0.5L GAZ","3 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("MINERALKA 1L GAZ","4 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("MINERALKA 1.5L GAZ","5 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("COCA COLA 400ML","5 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("COCA COLA 1.5L","11 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("COCA COLA 1L","9 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("COCA COLA 0.5","6 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("FANTA 400ML","5 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("FANTA 1.5L","11 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("FANTA 0.5L","6 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("FANTA 1L","9 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("SPRITE 0.5L","6 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("SPRITE 1.5L","11 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("SPRITE 400ML","5 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("ICE TEA","9 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("SOK 1L","10 000 UZS",R.drawable.drinks,"5.0","Becausel"))
-        listDrinks.add(LoookFood("SOK 1L (GRANAT,APELSIN,MALINA)","11 000 UZS",R.drawable.drinks,"5.0","Becausel"))
+        listDrinks.add(
+            LoookFood(
+                "MINERALKA 0.5L BEZGAZ",
+                "3 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+        listDrinks.add(
+            LoookFood(
+                "MINERALKA 1.5L BEZGAZ",
+                "5 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+        listDrinks.add(
+            LoookFood(
+                "MINERALKA 1L BEZGAZ",
+                "4 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+        listDrinks.add(
+            LoookFood(
+                "MINERALKA 0.5L GAZ",
+                "3 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+        listDrinks.add(
+            LoookFood(
+                "MINERALKA 1L GAZ",
+                "4 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+        listDrinks.add(
+            LoookFood(
+                "MINERALKA 1.5L GAZ",
+                "5 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+        listDrinks.add(
+            LoookFood(
+                "COCA COLA 400ML",
+                "5 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+        listDrinks.add(
+            LoookFood(
+                "COCA COLA 1.5L",
+                "11 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+        listDrinks.add(LoookFood("COCA COLA 1L", "9 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("COCA COLA 0.5", "6 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("FANTA 400ML", "5 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("FANTA 1.5L", "11 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("FANTA 0.5L", "6 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("FANTA 1L", "9 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("SPRITE 0.5L", "6 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("SPRITE 1.5L", "11 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("SPRITE 400ML", "5 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("ICE TEA", "9 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(LoookFood("SOK 1L", "10 000 UZS", R.drawable.drinks, "5.0", "Becasel"))
+        listDrinks.add(
+            LoookFood(
+                "SOK 1L (GRANAT,APELSIN,MALINA)",
+                "11 000 UZS",
+                R.drawable.drinks,
+                "5.0",
+                "Becasel"
+            )
+        )
+
+        /**
+         * Appetizers and Combo food
+         */
+        listAppetizersCombo.add(LoookFood("FRIES","9 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("WINGS","16 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("STRIPS","16 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("BITES","9 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("MUSHROOMS 6 PCS","9 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("1 WINGS","5 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("1 STRIPS","5 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("CHEESE NUGGETS","11 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("COMBO","12 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("WICKED COMBO (strips)","18 000 UZS",R.drawable.appr,"5.0","Becasel"))
+        listAppetizersCombo.add(LoookFood("WICKED COMBO (wings)","18 000 UZS",R.drawable.appr,"5.0","Becasel"))
+
 
 
 
---
+++ app/src/main/java/uz/unzosoft/fastfoodbecasel/data/ConstFoods.kt
@@ -0,0 +1,3 @@
+package uz.unzosoft.fastfoodbecasel.data
+
+import uz.unzosoft.fastfoodbecasel.ui.model.LoookFood
