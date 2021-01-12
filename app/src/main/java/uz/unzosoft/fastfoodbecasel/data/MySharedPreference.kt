429d67b8b1448f2fe14f0465c9352086f3082a10
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-01-12 22:38:20 +0500

    Splash screen

---
+++ app/src/main/java/uz/unzosoft/fastfoodbecasel/data/MySharedPreference.kt
@@ -0,0 +1,4 @@
+package uz.unzosoft.fastfoodbecasel.data
+
+class MySharedPreference {
+}
--- app/src/main/java/uz/unzosoft/fastfoodbecasel/ui/SplashActivity.kt
+++ app/src/main/java/uz/unzosoft/fastfoodbecasel/ui/SplashActivity.kt
@@ -1,13 +1,27 @@
 package uz.unzosoft.fastfoodbecasel.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
 import uz.unzosoft.fastfoodbecasel.R
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
+    var animationSplash: Animation? = null
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        animationSplash = AnimationUtils.loadAnimation(this, R.anim.splash_anim)
+        splashImage.animation = animationSplash
+        handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(this, IntroductoryActivity::class.java))
+            finish()
+        }, 4000)
     }
 }
